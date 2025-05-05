51c5fe59058d03b15c133b15c2be66bcedde70e1
notlim_dev <elmotigre6@gmail.com>
2025-05-04 19:37:24 -0500

    add module prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'prisma/prisma.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { CategoriesModule } from './categories/categories.module';
 import { TagsModule } from './tags/tags.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BookmarksModule, CategoriesModule, TagsModule, AuthModule],
+  imports: [PrismaModule, BookmarksModule, CategoriesModule, TagsModule, AuthModule],
 })
 export class AppModule { }
