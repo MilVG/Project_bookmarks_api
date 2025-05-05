9eeaeb93e58be063da797b54bcf904d373afaca2
notlim_dev <elmotigre6@gmail.com>
2025-05-04 19:38:24 -0500

    inject PrismaService  to bookmarks-service

--- src/bookmarks/bookmarks.service.ts
+++ src/bookmarks/bookmarks.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class BookmarksService {}
+export class BookmarksService {
+  constructor(private prisma: PrismaService) { }
+
+  async getAllBookmarks() {
+
+    return this.prisma.bookmarks.findMany(); // usando el modelo generado por Prisma
+  }
+}
