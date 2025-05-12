968fb077b4c0f51dc0f2087fb4aaa295ffe9fcf9
notlim_dev <elmotigre6@gmail.com>
2025-05-11 22:02:54 -0500

    add create category ==> service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(private prisma: PrismaService) { }
+  async create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.categories.create({
+      data: {
+        name: createCategoryDto.name,
+      },
+    });
+  }
+
+  // opcional: método para listar todas
+  async findAll() {
+    return this.prisma.categories.findMany();
+  }
+}
