8b4270041c0efa71f52e021cecd3970b49b664ed
notlim_dev <elmotigre6@gmail.com>
2025-05-11 22:05:12 -0500

    add endpoint POST: create bookmarks

--- src/bookmarks/bookmarks.controller.ts
+++ src/bookmarks/bookmarks.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BookmarksService } from './bookmarks.service';
+import { CreateBookmarkDto } from './dto/create-bookmark.dto';
 
 @Controller('bookmarks')
 export class BookmarksController {
@@ -10,4 +11,10 @@ export class BookmarksController {
   async getAllBookmarks() {
     return this.bookmarksService.getAllBookmarks()
   }
+  // Crear un marcador
+  @Post()
+  async createBookmark(@Body() createBookmarkDto: CreateBookmarkDto) {
+    return this.bookmarksService.createBookmark(createBookmarkDto);
+  }
+
 }
