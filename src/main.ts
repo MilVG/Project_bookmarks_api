89b8c3a4bd10d30e9503433564c3e08b5f20ad38
notlim_dev <elmotigre6@gmail.com>
2025-05-11 22:05:53 -0500

    add cors =>> all methods

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'chrome-extension://nhabnlmhbihebccklgpodopeocajdboa',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  });
+  app.useGlobalPipes(new ValidationPipe()); // habilita validación automática
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
