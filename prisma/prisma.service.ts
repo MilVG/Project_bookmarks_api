7321409e62ecae438147e36c1ee0f20eac77a6f4
notlim_dev <elmotigre6@gmail.com>
2025-05-12 15:17:59 -0500

    fix path prisma generate folder

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 
 // src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
