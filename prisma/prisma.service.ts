af7437934f5e5b3f85442c6af5419b10d4dfdc78
notlim_dev <elmotigre6@gmail.com>
2025-05-12 12:21:25 -0500

    Modify service adapter prisma and neon Adapter

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,17 +1,34 @@
+
+// src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { neonConfig } from '@neondatabase/serverless';
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaNeon({ connectionString });
+
+// @ts-ignore - Prisma aún no tipa correctamente 'adapter'
+const prismaClient = new PrismaClient({ adapter });
 
 @Injectable()
-export class PrismaService extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy {
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private readonly prisma = prismaClient;
 
-  //Conexion prisma con base de datos
   async onModuleInit() {
-    await this.$connect();
+    await this.prisma.$connect();
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.prisma.$disconnect();
+  }
+
+  // Delegamos métodos de PrismaClient para usar prismaService como si fuera prismaClient
+  get client() {
+    return this.prisma;
   }
 }
 
